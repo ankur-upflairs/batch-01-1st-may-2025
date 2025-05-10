bc9d1d7ce6fad55b39790bde67334b55a7719888
ankur-upflairs <ankur.upflairs@gmail.com>
2025-05-10 12:05:32 +0530

    operators

---
+++ javascript/operators.js
@@ -0,0 +1,37 @@
+//operators -> that performs some specific operation
+//arithmatic operators => +, -, /(divide) , * (multiply), %(modulus -> gives remainder)
+// console.log(10%3)
+//comparison operators-> compare 2 values and return boolean(true/false)
+//>(greater than), < (less than), >= (gt equal to ) , <= (lt equal to)
+// console.log(5 <= 7)
+// == (equal) , (!=)not equal
+// console.log(5==5)
+// console.log(5!=5)
+//=== (check value as well as data type)
+console.log(7=='7')
+console.log(7==='7')
+//assignment operators
+//=, +=, -=,/=, *=, %=
+var a= 5;
+a+=3 //a=a+3
+console.log(a)
+//logical operators=> compare two booleans and return boolean
+//logical and && => if both side values are true than return true , otherwise return false
+// console.log(true && false)
+// console.log(false && true)
+// console.log(true && true)
+// console.log(false && false)
+// console.log(3>2 && 5<8)//(true && true)
+// logical or || => if either side is true than return true , otherwise false
+// console.log(true || false) 
+// console.log(false || true)
+// console.log(true || true)
+// console.log(false || false)
+console.log(3>2 || 5>8)//
+//logical not ! => reverse the boolean value
+console.log(!true)
+
+
+
+
+
