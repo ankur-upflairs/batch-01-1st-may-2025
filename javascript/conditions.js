090088c95563261c5c8719af6dcbd7653c2b71f5
ankur-upflairs <ankur.upflairs@gmail.com>
2025-05-10 12:40:07 +0530

    fkdj

---
+++ javascript/conditions.js
@@ -0,0 +1,15 @@
+//if-else => execute a block of code based on certain conditions
+// https://www.npmjs.com/package/prompt-sync
+var prompt = require('prompt-sync')();
+// let a = 6
+// let b = 7
+// if(a>b){
+//     console.log('bigger than 7')
+// }
+// else{
+//     console.log('smaller than 7')
+// }
+
+var age = prompt('enter your name? ... ')
+console.log(age)
+
--- javascript/operators.js
+++ javascript/operators.js
@@ -8,13 +8,13 @@
 // console.log(5==5)
 // console.log(5!=5)
 //=== (check value as well as data type)
-console.log(7=='7')
-console.log(7==='7')
+// console.log(7=='7')
+// console.log(7==='7')
 //assignment operators
 //=, +=, -=,/=, *=, %=
 var a= 5;
 a+=3 //a=a+3
-console.log(a)
+// console.log(a)
 //logical operators=> compare two booleans and return boolean
 //logical and && => if both side values are true than return true , otherwise return false
 // console.log(true && false)
@@ -27,11 +27,15 @@ console.log(a)
 // console.log(false || true)
 // console.log(true || true)
 // console.log(false || false)
-console.log(3>2 || 5>8)//
+// console.log(3>2 || 5>8)//
 //logical not ! => reverse the boolean value
-console.log(!true)
-
-
+// console.log(!true)
+//++,-- => increment , decrement
+var a= 5;
+// a++;
+// console.log(a)
+// a--
+console.log(++a)
 
 
 
