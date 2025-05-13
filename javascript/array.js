1f046ebffa8e35445263cd4430b4f786b928010b
ankur-upflairs <ankur.upflairs@gmail.com>
2025-05-13 12:20:05 +0530

    array

---
+++ javascript/array.js
@@ -0,0 +1,25 @@
+// let a= [2,'a',true]
+// console.log(a[1])
+//normal / iterative methods
+
+let b = [3,4,6,7,8]
+// console.log(b.concat([4,6]))
+// console.log(b.includes(6))
+// console.log(b.reverse())//mutate
+// console.log(b.pop())
+// console.log(b.push(9))
+// console.log(b.shift())
+// console.log(b.unshift(1))
+// console.log(b.slice(0,2))
+// console.log(b.splice(1,1))
+// console.log(b.splice(1,0,8,9))
+// console.log(b)
+// let c= b.join('')
+// console.log(b.join(''))
+// console.log(typeof c)
+
+//take five values from user and save them in a array
+//ask name from user - > replace  first and last with $
+// ask user for enter the values of an array than ask to remove the
+// index and value which he wants to replace 
+
