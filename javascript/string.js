6ea88a007377974d787832dc3f3f27bfc6cf81fb
ankur-upflairs <ankur.upflairs@gmail.com>
2025-05-13 11:43:44 +0530

    string

---
+++ javascript/string.js
@@ -0,0 +1,39 @@
+let a = "hello"
+// console.log(a)
+
+// template string / string literal 
+//multiline string
+let b = `my name 
+is ankur singh`
+let c = `${a} my name 
+is ankur singh`
+// console.log(c)
+
+//string is immutable 
+// console.log(a.at(1))
+// console.log(a[1])
+// console.log(a.length)
+// console.log(a.includes('el'))
+// console.log(a.concat(' everybuddy'))
+// console.log(a.endsWith('o'))
+// console.log(a.indexOf('l'))
+// console.log(a.lastIndexOf('l'))
+//trim - remove spaces
+let d= "    xyz"
+// console.log(d)
+// console.log(d.trim())
+// let e='abc'
+// console.log(e.padStart(10,'*'))
+// console.log(a.replace('l','x'))
+// console.log(a.replaceAll('l','x'))
+// console.log(a.slice(3))
+// console.log(a.split(''))
+// console.log(b.split(' '))
+
+//take a input from user and find it includes 'a'
+//if 'a' included than print a is included 
+// ask user for their name and fit that name in greeting 
+//sentance
+//input - replace vowels with 'x'
+
+
