fea720f819e8b19187033224ff1c038e753e7cfd
ankur-upflairs <ankur.upflairs@gmail.com>
2025-06-03 11:34:11 +0530

    node basics

---
+++ node/Basics/module2.js
@@ -0,0 +1,8 @@
+let x = 23;
+
+// module.exports = x
+
+exports.x=x
+
+
+
---
+++ node/Basics/moduleSystem.js
@@ -0,0 +1,31 @@
+// let a= 'hi , how r u ?'
+// console.log(a)
+
+// own module , built-in module , 3rd party module 
+
+//cjs => common js
+// const mfile = require('./module2')
+
+// console.log(mfile.x)
+
+//EJS =>
+
+// import { x } from "./module1.js";
+// console.log(x)
+
+// import os from 'node:os'
+// console.log(`Total memory of my system is:- ${os.totalmem()}
+// and free memory is - ${os.freemem()}`)
+// console.log(os.version())
+
+
+
+// import colors from 'colors'
+
+// console.log(colors.bgGreen.red('hello everyone'))
+// console.log()
+
+//os 5 function 
+//one third party module :- work with strings 
+
+
