9b5f35a1761b365c47b1b20f74ef5b800326b3e8
ankur-upflairs <ankur.upflairs@gmail.com>
2025-06-09 11:40:44 +0530

    crud

--- node/task management- db/controllers/taskController.js
+++ node/task management- db/controllers/taskController.js
@@ -1,65 +1,66 @@
+import { Task } from "../models/tasks.js";
 
-import {Task} from '../models/tasks.js'
-
-
-
-export const getAllTasks = (req, res) => {
-  // let tasks = getFileContent();
-  // res.status(200).json({ success: true, tasks: tasks });
+export const getAllTasks = async (req, res) => {
+  try {
+    let tasks = await Task.find({});
+    res.status(200).json({ success: true, tasks });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
 };
 
-export const getSingleTask = (req, res) => {
-  // let { id } = req.params;
-  // let tasks = getFileContent();
-
-  // let task = tasks.find((v) => v.id == id);
-  // if (!task) {
-  //   return res.json({
-  //     success: false,
-  //     message: "task not found",
-  //   });
-  // }
-  // res.status(200).json({ success: true, task: task });
+export const getSingleTask = async (req, res) => {
+  try {
+    let { id } = req.params;
+    let task = await Task.findById(id);
+    if (!task) {
+      return res.json({
+        success: false,
+        message: "task not found",
+      });
+    }
+    res.status(200).json({ success: true, task: task });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
 };
 
 export const createTask = async (req, res) => {
   // console.log(req.body)
   try {
     let { title, description, dueDate } = req.body;
-    await Task.create({title, description, dueDate})
-    res.json({ success: true, message: "New Task added" });    
+    let task = new Task({ title, description, dueDate });
+    await task.save()
+    // await Task.create({ title, description, dueDate });
+    res.json({ success: true, message: "New Task added" });
   } catch (error) {
-    console.log(error)
+    console.log(error);
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export const updateTask = async (req, res) => {
+  try {
+    let { id } = req.params;
+    let { title, description, dueDate } = req.body;
+    let task = await Task.findByIdAndUpdate(id,{ title, description, dueDate })
+   
+    res.json({ success: true, message: `Task updated`,task });
+    
+  } catch (error) {
     res.json({ success: false, message: error.message });
     
   }
-  // try {
-  //   let { title, description, dueDate } = req.body;
-  //   let task = new Task({title,description,dueDate})
-  //   await task.save()
-  //   res.json({ success: true, message: "New Task added" });    
-  // } catch (error) {
-  //   console.log(error)
-  //   res.json({ success: false, message: error.message });    
-  // }
 };
 
-export const updateTask = (req, res) => {
-  // let { id } = req.params;
-  // let { title, description, dueDate } = req.body;
-  // let tasks = getFileContent();
-  // let index = tasks.findIndex(v=>v.id == id)  
-  //   tasks[index].title = title;
-  //   tasks[index].description = description;
-  //   tasks[index].dueDate = dueDate;      
-  // writeData(tasks);
-  // res.json({ success: true, message: `Task updated with id - ${id}` });
-};
-
-export const deleteTask = (req, res) => {
-  // let { id } = req.params;
-  // let tasks = getFileContent();
-  // let updatedTask = tasks.filter((v, i) =>  v.id != id);
-  // writeData(updateTask);
-  // res.json({ success: true, message: `Task deleted with id - ${id}` });
+export const deleteTask = async (req, res) => {
+  try {
+    let { id } = req.params;
+    await Task.findByIdAndDelete(id)
+    res.json({ success: true, message: `Task deleted with id - ${id}` });
+    
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+    
+  }  
 };
